63ac3e27ab23e1dd9196fd39d1a1264e820eae99
MuhammedBasith <iambasith123@gmail.com>
2024-10-21 12:50:48 +0530

    first commit

---
+++ src/componenets/LogisticsDashboard.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../App.css';
+import WarehouseInventory from './WarehouseInventory';
+import DeliveryStatus from './DeliveryStatus';
+
+const LogisticsDashboard: React.FC = () => {
+  const [warehouseItems, setWarehouseItems] = useState<number>(100);
+  const [isTruckLeft, setIsTruckLeft] = useState<boolean>(false);
+  const [dispatchQuantity, setDispatchQuantity] = useState<number>(0);
+
+  const handleTruckDeparture = () => {
+    if (dispatchQuantity > 0 && dispatchQuantity <= 20 && warehouseItems >= dispatchQuantity && warehouseItems - dispatchQuantity >= 20) {
+      setWarehouseItems(warehouseItems - dispatchQuantity);
+      // setIsTruckLeft(true);
+      setDispatchQuantity(0);
+    }
+  };
+
+  return (
+    <div className="dashboard">
+      <h1>Logistics Dashboard</h1>
+      <div className="dashboard-content">
+        <WarehouseInventory itemCount={warehouseItems} />
+        <DeliveryStatus 
+          isTruckLeft={isTruckLeft} 
+          onTruckDeparture={handleTruckDeparture} 
+          dispatchQuantity={dispatchQuantity}
+          setDispatchQuantity={setDispatchQuantity}
+          warehouseItems={warehouseItems}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LogisticsDashboard;
